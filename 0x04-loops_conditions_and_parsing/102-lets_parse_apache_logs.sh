58c52b2c6d6472ebb65c4a1ac3f1f976845fbb09
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-31 23:28:48 +0300

    102-lets_parse_apache_logs

--- 0x04-loops_conditions_and_parsing/102-lets_parse_apache_logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Path to the Apache log file
-log_file="apache-access.log"
-
-# Use awk to extract IP and HTTP status code, then group and sort
-awk '{print $1, $9}' "$log_file" | sort | uniq -c | sort -nr | awk '{print $1, $2, $3}'
