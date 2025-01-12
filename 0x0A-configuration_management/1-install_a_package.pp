a9be386fa88cdfef15a13ce321ca05cff5cb38ea
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-01-12 13:17:01 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # This Puppet manifest installs Flask version 2.1.0 using pip3.
-package { 'python3-pip':
-    ensure => installed,
-} ->
-exec { 'pip3 install flask==2.1.0':
-    command => '/usr/bin/pip3 install flask==2.1.0',
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
