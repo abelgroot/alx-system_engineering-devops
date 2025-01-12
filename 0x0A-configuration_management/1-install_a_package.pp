5b4340ebef3b15fd20de597148d6c070f10e5605
groot <abel.mulugeta.dagne@gmail.com>
2025-01-12 18:11:22 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,11 @@ package { 'python3-pip':
   ensure => 'installed',
 }
 
+package { 'werkzeug':
+    ensure => '2.2.2',
+    provider => 'pip3',
+}
+
 # Install Flask 2.1.0 using pip3
 exec { 'Install Flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
