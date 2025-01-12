cb9607ac55f13abeca819137dcbe1efec2da62ed
groot <abel.mulugeta.dagne@gmail.com>
2025-01-12 18:32:20 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
-# Get the home directory of the current user
-# Puppet manifest to configure SSH client settings
-
-# Ensure private key usage is declared
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => 'IdentityFile',
+# Add IdentityFile to ssh_config if not present
+exec { 'Set IdentityFile':
+  command => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  unless  => 'grep -q "IdentityFile ~/.ssh/school" /etc/ssh/ssh_config',
+  path    => ['/usr/bin', '/bin'],
 }
 
-# Ensure password authentication is disabled
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => 'PasswordAuthentication',
+# Add PasswordAuthentication to ssh_config if not present
+exec { 'Disable PasswordAuthentication':
+  command => 'echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  unless  => 'grep -q "PasswordAuthentication no" /etc/ssh/ssh_config',
+  path    => ['/usr/bin', '/bin'],
 }
