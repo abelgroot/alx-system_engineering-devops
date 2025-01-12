fea0d464fafff4a6dcd5ac8f8d6c47895e59e628
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-01-12 12:35:16 +0300

    1-install_a_package.pp updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,7 @@
 # This Puppet manifest installs Flask version 2.1.0 using pip3.
 package { 'python3-pip':
     ensure => installed,
-}
-
-# Install Flask 2.1.0 using an exec resource
-package { 'flask':
-    ensure => installed,
-    name => 'flask~=2.1.0',
-    provider => 'pip3',
+} ->
+exec { 'pip3 install flask==2.1.0':
+    command => '/usr/bin/pip3 install flask==2.1.0',
 }
