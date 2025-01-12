239ddb2733c7785c7af3b4771091400912004c1f
abel mulugeta <abelmulugeta@abels-MacBook-Pro.local>
2025-01-12 18:16:33 +0300

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,17 @@
 # Ensure pip3 is installed
 package { 'python3-pip':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-package { 'werkzeug':
-  ensure => '2.2.2',
-  provider => 'pip3',
+# Install Werkzeug version 2.2.2 using pip3
+exec { 'Install Werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.2.2',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.2.2"',
+  require => Package['python3-pip'],
 }
 
-# Install Flask 2.1.0 using pip3
+# Install Flask version 2.1.0 using pip3
 exec { 'Install Flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin', '/bin'],
