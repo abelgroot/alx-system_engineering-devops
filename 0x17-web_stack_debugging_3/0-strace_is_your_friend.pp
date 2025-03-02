8887eacf26c1b46922a6904e031eb1184af2a4c5
groot <abel.mulugeta.dagne@gmail.com>
2025-03-02 18:08:10 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,10 +3,4 @@
 exec { 'fix_wp_settings':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/usr/local/bin/:/bin/'
-    notify  => Exec['restart_apache'],
-}
-# Restart Apache after applying the fix
-exec { 'restart_apache':
-    command     => '/usr/sbin/apachectl restart',
-    refreshonly => true,  # Only restart if notified
 }
