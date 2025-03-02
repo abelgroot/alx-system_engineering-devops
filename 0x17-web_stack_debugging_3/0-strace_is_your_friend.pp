ac028b25a03250d9c50d50f7562cd8b6bb15e7ed
groot <abel.mulugeta.dagne@gmail.com>
2025-03-02 17:55:51 +0300

    update the hole code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
 # Puppet manifest to fix incorrect filename reference in wp-settings.php
-# This script replaces "class-wp-locale.phpp" with "class-wp-locale.php"
-# and restarts Apache to apply changes.
 
 exec { 'fix_wp_settings':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
