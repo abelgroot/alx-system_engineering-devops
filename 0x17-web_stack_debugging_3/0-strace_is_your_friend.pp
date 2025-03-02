91ea9b2d64c050c90906e4e259ed98668f6ef6de
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-03-02 16:50:48 +0300

    update comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
+# Puppet manifest to fix incorrect filename reference in wp-settings.php
+# This script replaces "class-wp-locale.phpp" with "class-wp-locale.php"
+# ensuring WordPress can correctly locate the required file.
 exec { 'fix_wp_settings':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
   onlyif  => "grep -q 'class-wp-locale.phpp' /var/www/html/wp-settings.php",
