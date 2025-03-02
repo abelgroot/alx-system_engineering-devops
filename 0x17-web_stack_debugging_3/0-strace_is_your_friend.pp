a0c0f361b5a5daa31cf12888a9735c7d734f408c
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-03-02 17:27:42 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,11 +3,8 @@
 # and restarts Apache to apply changes.
 
 exec { 'fix_wp_settings':
-  command     => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-  
-  # Fully qualified path for grep to avoid errors
-  onlyif      => "/bin/grep -q 'class-wp-locale.phpp' /var/www/html/wp-settings.php",
-  
+  command     => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/bin/:/bin/'
   # Ensure the Apache service is restarted after fixing the issue
   notify      => Exec['restart_apache'],
 }
