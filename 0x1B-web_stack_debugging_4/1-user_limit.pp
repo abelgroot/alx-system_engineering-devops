76cf6228e1ceea3b82c22878f19a24e099f9c5e5
groot <abel.mulugeta.dagne@gmail.com>
2025-03-03 23:57:29 +0300

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,32 +1,13 @@
-# Puppet manifest to increase the file descriptor limit for the holberton user
+# Puppet manifest to increase the file descriptor limit for the holberton user using sed
 
-# Update the /etc/security/limits.conf file to increase the file descriptor limit for the holberton user
-file_line { 'increase_holberton_soft_limit':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton soft nofile 65536',
-  match  => '^holberton soft nofile',
+# Update the soft limit for the holberton user in /etc/security/limits.conf
+exec { 'update_holberton_soft_limit':
+  command => "sed -i '/^holberton\\s\\+soft\\s\\+nofile/c\\holberton soft nofile 65536' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin'],  # Ensure sed is in the PATH
 }
 
-file_line { 'increase_holberton_hard_limit':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton hard nofile 65536',
-  match  => '^holberton hard nofile',
-}
-
-# Ensure the PAM limits module is configured to use /etc/security/limits.conf
-file_line { 'enable_pam_limits':
-  ensure => present,
-  path   => '/etc/pam.d/common-session',
-  line   => 'session required pam_limits.so',
-  match  => '^session\s+required\s+pam_limits\.so',
-}
-
-# Apply the changes immediately for the current session
-exec { 'apply_limits':
-  command     => 'sysctl -p /etc/security/limits.conf',
-  path        => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => [File_line['increase_holberton_soft_limit'], File_line['increase_holberton_hard_limit']],
+# Update the hard limit for the holberton user in /etc/security/limits.conf
+exec { 'update_holberton_hard_limit':
+  command => "sed -i '/^holberton\\s\\+hard\\s\\+nofile/c\\holberton hard nofile 65536' /etc/security/limits.conf",
+  path    => ['/bin', '/usr/bin'],  # Ensure sed is in the PATH
 }
