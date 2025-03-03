a75d9a72616266afb5e3555b46764a3abb75c80f
groot <abel.mulugeta.dagne@gmail.com>
2025-03-03 18:51:05 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,14 +2,8 @@
 
 exec { 'fix_apache2':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/:/bin/',
+    path    => '/bin:/usr/local/bin/',
     notify  => Exec['restart_apache'],
-}
-
-exec { 'fix_mysql':
-    command => 'sed -i s/myisam-recover/myisam-recover-options/g /etc/mysql/my.cnf',
-    path    => '/usr/bin:/usr/sbin/',
-    notify  => Exec['restart_mysql'],
 }
 
 # Restart Apache after applying the fix
@@ -17,8 +11,3 @@ exec { 'restart_apache':
     command => '/usr/sbin/apachectl restart',
 }
 
-# Restart mysql after applying the fix
-exec { 'restart_mysql':
-    command => 'service mysql restart',
-    path    => '/usr/bin:/usr/sbin/',
-}
